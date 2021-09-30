8e3fe1fec2938254699c7487f3a6e84add89e3a0
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-30 12:14:39 +0330

    modify files
    Co-Authored-By: Tayebeh Safdari <66350369+tayebehsafdari@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,17 +1,39 @@
 import {StatusBar} from 'expo-status-bar';
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, Button, TouchableOpacity, Image, TextInput, ScrollView} from 'react-native';
 
 export default function App() {
     const [count, setCount] = useState(0);
     return (
-        <View style={styles.container}>
-            <TouchableOpacity onPress={() => setCount(count + 1)} style={styles.button}>
-                <Text>Click me!</Text>
-            </TouchableOpacity>
-            <Text style={{marginBottom: 10}}>You clicked {count} times</Text>
-            <Button title="Click me!" onPress={() => setCount(count + 1)}/>
-        </View>
+        // <View style={styles.container}>
+        //     <TouchableOpacity onPress={() => setCount(count + 1)} style={styles.button}>
+        //         <Text>Click me!</Text>
+        //     </TouchableOpacity>
+        //     <Text style={{marginBottom: 10}}>You clicked {count} times</Text>
+        //     <Button title="Click me!" onPress={() => setCount(count + 1)}/>
+        // </View>
+        <ScrollView>
+            <Text>Some text</Text>
+            <View>
+                <Text>Some more text</Text>
+                <Image
+                    source={{
+                        uri: 'https://reactnative.dev/docs/assets/p_cat2.png'
+                    }}
+                    style={{
+                        width: 200,
+                        height: 200
+                    }}
+                />
+            </View>
+            <TextInput defaultValue="You can type in me"
+                       style={{
+                           borderColor: 'gray',
+                           borderWidth: 1,
+                           height: 40
+                       }}
+            />
+        </ScrollView>
     );
 }
 
